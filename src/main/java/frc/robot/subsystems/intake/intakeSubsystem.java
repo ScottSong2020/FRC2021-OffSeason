5be8476c5795ada0eaf9fa099063b53aeef95f81
Ibro-cyber <71227338+Ibro-cyber@users.noreply.github.com>
2021-10-03 16:45:56 -0700

    Added Buttons and Motor for Intake Subsystem (#13)

--- src/main/java/frc/robot/subsystems/intake/intakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/intakeSubsystem.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeSubsystem {
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.utils.PS4Constants;
+
+public class intakeSubsystem {
+
+    private static final String operatorControl = null;
+    private WPI_TalonSRX intake;
+    public void initialize() {
+        intake = new WPI_TalonSRX(1);
+        
+    }
     
+    POVButton operatorIntake = new POVButton(operatorControl, 180); // down
+    POVButton operatorOutake = new POVButton(operatorControl, 0); // up
+    JoystickButton operatorToggleIntake = new JoystickButton(operatorControl, PS4Constants.R1.getId());
+
 }

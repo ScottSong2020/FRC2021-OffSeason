450e414b6bc64a091e05bd295e595da9480c644e
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-10-03 16:12:18 -0700

    Buttons.java file

---
+++ src/main/java/frc/robot/Buttons.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import frc.robot.utils.PS4Constants;
+
+public class Buttons {
+
+    Joystick driverControl = new Joystick(0);
+    Joystick operatorControl = new Joystick(1);
+    
+}

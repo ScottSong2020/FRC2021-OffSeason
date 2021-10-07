e6b92187f99d4d26199b31107e4b204d1eea6e29
Saptarshi Mallick <sapmallick@gmail.com>
2021-10-06 20:17:37 -0700

    Added custom implementation of SubsystemBase: BitBucketsSubsystem.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,16 +2,30 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.BitBucketsSubsystem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Robot extends TimedRobot {
 
+    public static final List<BitBucketsSubsystem> ROBOT_SUBSYSTEMS = new ArrayList<>();
+
     @Override
     public void robotInit() {
+
+
+
+        //Initialize all subsystems (do this AFTER subsystem objects are created and instantiated)
+        ROBOT_SUBSYSTEMS.forEach(BitBucketsSubsystem::init);
     }
 
     @Override
     public void robotPeriodic() {
-        // always run the CommandScheduler during periodic
+
+        //Run periodic function on all subsystems each time robotPeriodic is called
+        ROBOT_SUBSYSTEMS.forEach(BitBucketsSubsystem::periodic);
+
         CommandScheduler.getInstance().run();
     }
 
